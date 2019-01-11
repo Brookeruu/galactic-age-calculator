cd2dd7ffa8be507dbbcdf65c1b8de79829ad1507
Brooke <perrysbrooke@gmail.com>
2019-01-11 09:11:29 -0800

    add 1st spec to input & rtn a number

---
+++ spec/age-calculator-spec.js
@@ -0,0 +1,14 @@
+import { AgeCalculator } from './../age-calculator.js';
+
+describe ('AgeCalculator', function() {
+  let ageEarth;
+
+  before(function () {
+    ageEarth = new AgeCalculator(30);
+  })
+
+  it('should take in and return a number' function(
+    expect(ageEarth).toEqual(30);
+  ))
+
+})
